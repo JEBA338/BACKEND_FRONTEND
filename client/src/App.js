314c79dbc16f29c916abf7a6a5f2e23baffb3b7e
Joseph Bravo <josephedu1994@gmail.com>
2025-08-29 23:50:09 -0500

    my first commint from frontend

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       HOLA MUNDO 
+      </header>
+    </div>
+  );
+}
+
+export default App;
